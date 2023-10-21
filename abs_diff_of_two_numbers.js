514e3ef510050a734bc5b4f038b53a5e11e802af
Arnab Adhikary <aarnab11@gmail.com>
2023-10-21 18:29:20 +0530

    Containing two functions, One is to find the difference and the other one is to find the absolute value of a number

---
+++ abs_diff_of_two_numbers.js
@@ -0,0 +1,19 @@
+function diffOfTwoNum(a,b) {
+    let diff = (a-b)
+
+    console.log(diff)   
+}
+
+function absValue(n) {
+   if(n<0)
+   {
+    console.log(-n)
+   }
+   else
+   {
+    console.log(n)
+   } 
+}
+
+diffOfTwoNum(-14,10)
+absValue(-100)
